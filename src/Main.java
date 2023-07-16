cda1a3f146c7b5aa2ab3cab8116362f3db0bcd2c
Albina <albina.melsitova@mail.ru>
2023-07-16 15:12:40 +0300

    Task about threads

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Пользователь вводит с клавиатуры значение в массив.
  * После чего запускаются два потока.
